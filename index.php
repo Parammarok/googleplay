8a96d208ff901d639b5696e9384f7cff8d4b00a4
ParaMMarok <rockingboy3600@gmail.com>
2022-03-24 08:29:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if(!is_dir($directoryName)){
 }
 
 //The name of the directory that we need to create.
-$directoryName = '.cache//googleplay';
+$directoryName = '.cache/googleplay';
 
 //Check if the directory already exists.
 if(!is_dir($directoryName)){
