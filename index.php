b6bc5b687491ccc69cfcbce94068423ee3e89d8b
ParaMMarok <rockingboy3600@gmail.com>
2022-03-24 08:28:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,5 +12,15 @@ if(!is_dir($directoryName)){
     echo 'Folder exist';
 }
 
+//The name of the directory that we need to create.
+$directoryName = '.cache//googleplay';
 
+//Check if the directory already exists.
+if(!is_dir($directoryName)){
+    //Directory does not exist, so lets create it.
+    mkdir($directoryName, 0777);
+    echo 'Folder Created';
+} else {
+    echo 'Folder exist';
+}
 ?>
